bae2d1d221aa8e725261b9e6a4c03629d38f1d82
Matan Balestra <matan.balestra@gmail.com>
2025-01-26 13:59:13 +0200

    add rated

--- src/components/StarRating/index.tsx
+++ src/components/StarRating/index.tsx
@@ -5,19 +5,20 @@ import {FaStar} from 'react-icons/fa';
 import './styles.css';
 
 interface IProps { 
-    numberOfStars?: number, 
-    size?: number, 
-    defaultColor?: string, 
-    highlighColor?: string 
+    numberOfStars?: number; 
+    rate?: number;
+    size?: number;
+    defaultColor?: string;
+    highlighColor?: string;
     onRate?: (rating: number) => void; // Callback when the user clicks a star
     onHover?: (hoverRating: number) => void; // Callback when the user hovers over a star
     onHoverLeave?: () => void; // Callback when the user stops hovering
 }
 
 const StarRating = (props: IProps) => {
-    const { numberOfStars = 10, size = 40, defaultColor = '#e4e5e9', highlighColor = '#ffc107', onRate, onHover, onHoverLeave } = props;
+    const { numberOfStars = 10, rate = 0, size = 40, defaultColor = '#e4e5e9', highlighColor = '#ffc107', onRate, onHover, onHoverLeave } = props;
 
-    const [rating, setRating] = useState(0);
+    const [rating, setRating] = useState(rate);
     const [hover, setHover] = useState(0);
 
     const handleMouseClick = (currentIndex: number) => {
